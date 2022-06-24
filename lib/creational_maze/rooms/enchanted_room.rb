b9d5237622b07807609945df37ab2ac8d8d05a95
Mateus Pereira <mateus@ombulabs.com>
2022-06-23 22:47:46 -0300

    Adds initializer method to room classes

--- lib/creational_maze/rooms/enchanted_room.rb
+++ lib/creational_maze/rooms/enchanted_room.rb
@@ -1,5 +1,10 @@
 module Rooms
   class EnchantedRoom < Room
     attr_accessor :has_enchantment
+
+    def initialize(room_no)
+      super(room_no)
+      @has_enchantment = true
+    end
   end
 end
--- lib/creational_maze/rooms/room_with_a_bomb.rb
+++ lib/creational_maze/rooms/room_with_a_bomb.rb
@@ -1,5 +1,11 @@
 module Rooms
   class RoomWithABomb < Room
     attr_accessor :has_bomb, :has_gone_off
+
+    def initialize(room_no)
+      super(room_no)
+      @has_bomb = true
+      @has_gone_off = false
+    end
   end
 end
