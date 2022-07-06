49b6fced49e3aee83eee33f508b14696cdedec08
Mateus Pereira <mateus@ombulabs.com>
2022-07-06 16:39:17 -0300

    Refactors Maze class

--- lib/creational_maze/maze.rb
+++ lib/creational_maze/maze.rb
@@ -1,19 +1,23 @@
 module CreationalMaze
   class Maze
-    attr_accessor :rooms
+    attr_accessor :rooms_hash
 
     def initialize
-      @rooms = {}
+      @rooms_hash = {}
     end
 
     def enter; end
 
     def add_room(room)
-      @rooms[room.room_number] = room
+      @rooms_hash[room.room_number] = room
     end
 
     def room_no(room_number)
-      @rooms[room_number]
+      @rooms_hash[room_number]
+    end
+
+    def rooms
+      @rooms_hash.values
     end
   end
 end
