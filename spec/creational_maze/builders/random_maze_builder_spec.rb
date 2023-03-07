46f592a8c88ee36d88d2ab7693fa99f2bd06c2e6
Mateus Pereira <mateus@ombulabs.com>
2023-03-07 19:51:35 -0300

    Adds and refactors specs

--- spec/creational_maze/builders/random_maze_builder_spec.rb
+++ spec/creational_maze/builders/random_maze_builder_spec.rb
@@ -1,109 +1,103 @@
 require "spec_helper"
 
 RSpec.describe Builders::RandomMazeBuilder do
-  context "initialization" do
+  describe "initialization" do
     it "provides an array of factories" do
       rmb = Builders::RandomMazeBuilder.new
 
       expect(rmb.factories).to be_an(Array)
       expect(rmb.factories).not_to be_empty
-      expect(rmb.factories).to all(respond_to(:make_maze))
-      expect(rmb.factories).to all(respond_to(:make_wall))
-      expect(rmb.factories).to all(respond_to(:make_room))
-      expect(rmb.factories).to all(respond_to(:make_door))
     end
   end
 
-  context "usage" do
-    describe "#build_maze" do
-      it "creates a new maze for the builder" do
+  describe "#build_maze" do
+    it "creates a new maze for the builder" do
+      rmb = Builders::RandomMazeBuilder.new
+      rmb.build_maze
+
+      expect(rmb.current_maze).not_to be_nil
+      expect(rmb.current_maze).to be_a(CreationalMaze::Maze)
+    end
+  end
+
+  describe "#build_room" do
+    context "given there is no current maze" do
+      it "raises an error" do
+        rmb = Builders::RandomMazeBuilder.new
+
+        expect { rmb.build_room(1) }.to raise_error do |error|
+          expect(error).to be_a(Builders::Errors::InvalidStateError)
+        end
+      end
+    end
+
+    context "given there is a current maze" do
+      it "adds a room to the current maze" do
         rmb = Builders::RandomMazeBuilder.new
         rmb.build_maze
 
-        expect(rmb.current_maze).not_to be_nil
-        expect(rmb.current_maze).to be_a(CreationalMaze::Maze)
+        expect { rmb.build_room(1) }.to change { rmb.current_maze.rooms }
       end
     end
+  end
 
-    describe "#build_room" do
-      context "given there is no current maze" do
-        it "raises an error" do
-          rmb = Builders::RandomMazeBuilder.new
+  describe "#build_door" do
+    context "given there is no current maze" do
+      it "raises an error" do
+        rmb = Builders::RandomMazeBuilder.new
 
-          expect { rmb.build_room(1) }.to raise_error do |error|
-            expect(error).to be_a(Builders::Errors::InvalidStateError)
-          end
-        end
-      end
-
-      context "given there is a current maze" do
-        it "adds a room to the current maze" do
-          rmb = Builders::RandomMazeBuilder.new
-          rmb.build_maze
-
-          expect { rmb.build_room(1) }.to change { rmb.current_maze.rooms }
+        expect { rmb.build_door(Rooms::EnchantedRoom.new(1), Rooms::RoomWithABomb.new(2)) }.to raise_error do |error|
+          expect(error).to be_a(Builders::Errors::InvalidStateError)
         end
       end
     end
 
-    describe "#build_door" do
-      context "given there is no current maze" do
-        it "raises an error" do
-          rmb = Builders::RandomMazeBuilder.new
+    context "given the maze has no rooms" do
+      it "raises an error" do
+        rmb = Builders::RandomMazeBuilder.new
+        rmb.build_maze
 
-          expect { rmb.build_door(Rooms::EnchantedRoom.new(1), Rooms::RoomWithABomb.new(2)) }.to raise_error do |error|
-            expect(error).to be_a(Builders::Errors::InvalidStateError)
-          end
+        expect { rmb.build_door(Rooms::EnchantedRoom.new(1), Rooms::RoomWithABomb.new(2)) }.to raise_error do |error|
+          expect(error).to be_a(Builders::Errors::InvalidStateError)
         end
       end
+    end
 
-      context "given the maze has no rooms" do
-        it "raises an error" do
-          rmb = Builders::RandomMazeBuilder.new
-          rmb.build_maze
+    context "given a maze with at least two rooms" do
+      it "adds a door to both rooms" do
+        rmb = Builders::RandomMazeBuilder.new
+        rmb.build_maze
+        rmb.build_room(1)
+        rmb.build_room(2)
+        rmb.build_door(1, 2)
 
-          expect { rmb.build_door(Rooms::EnchantedRoom.new(1), Rooms::RoomWithABomb.new(2)) }.to raise_error do |error|
-            expect(error).to be_a(Builders::Errors::InvalidStateError)
-          end
-        end
+        room1 = rmb.current_maze.room_no(1)
+        room2 = rmb.current_maze.room_no(2)
+
+        expect(room1.sides.values).to include(a_kind_of(Doors::Door))
+        expect(room2.sides.values).to include(a_kind_of(Doors::Door))
       end
 
-      context "given a maze with at least two rooms" do
-        it "adds a door to both rooms" do
-          rmb = Builders::RandomMazeBuilder.new
+      context "given the door was added" do
+        let(:rmb) { Builders::RandomMazeBuilder.new }
+
+        before :each do
           rmb.build_maze
           rmb.build_room(1)
           rmb.build_room(2)
           rmb.build_door(1, 2)
+        end
 
+        it "adds the door to a common wall" do
           room1 = rmb.current_maze.room_no(1)
           room2 = rmb.current_maze.room_no(2)
 
-          expect(room1.sides.values).to include(a_kind_of(Doors::Door))
-          expect(room2.sides.values).to include(a_kind_of(Doors::Door))
-        end
+          door_to_2 = room1.get_doors.first
+          door_to_1 = room2.get_doors.first
+          direction_of_room2 = room1.get_direction(door_to_2)
+          direction_of_room1 = room2.get_direction(door_to_1)
 
-        context "given the door was added" do
-          let(:rmb) { Builders::RandomMazeBuilder.new }
-
-          before :each do
-            rmb.build_maze
-            rmb.build_room(1)
-            rmb.build_room(2)
-            rmb.build_door(1, 2)
-          end
-
-          it "adds the door to a common wall" do
-            room1 = rmb.current_maze.room_no(1)
-            room2 = rmb.current_maze.room_no(2)
-
-            door_to_2 = room1.get_doors.first
-            door_to_1 = room2.get_doors.first
-            direction_of_room2 = room1.get_direction(door_to_2)
-            direction_of_room1 = room2.get_direction(door_to_1)
-
-            expect(direction_of_room2).to eq(rmb.opposite_direction(direction_of_room1))
-          end
+          expect(direction_of_room2).to eq(rmb.opposite_direction(direction_of_room1))
         end
       end
     end
